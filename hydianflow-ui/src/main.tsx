2b8e81ca1348c20fe30e73e630e7fa47adc85b21
Austin Merrill <austinm9506@gmail.com>
2025-09-20 03:00:56 -0400

    Removed api config from App.tsx and moved to main.tsx.

--- hydianflow-ui/src/main.tsx
+++ hydianflow-ui/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { api } from "@/lib/api"
 import './index.css'
 import App from './App.tsx'
+
+api.setBaseURL(import.meta.env.VITE_API_BASE_URL ?? "");
+api.setWithCredentials(true);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
