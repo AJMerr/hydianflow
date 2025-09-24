b504024c0136839606ffcc4d85dcfde65ad78d89
Austin Merrill <austinm9506@gmail.com>
2025-09-24 01:27:46 -0400

    Added Toaster from sonner.

--- hydianflow-ui/src/main.tsx
+++ hydianflow-ui/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { api } from "@/lib/api"
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from './components/ui/sonner.tsx'
 
 api.setBaseURL(import.meta.env.VITE_API_BASE_URL ?? "");
 api.setWithCredentials(true);
@@ -10,5 +11,6 @@ api.setWithCredentials(true);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster richColors closeButton position="top-right" />
   </StrictMode>,
 )
